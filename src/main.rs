6d0ab4ee85ecfa1ea52124f16627dd652acd7530
Filip Szczepański <jazz2rulez@gmail.com>
2018-05-01 18:27:16 +0200

    A bit of refactoring

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,13 @@ use minifb::{Key, Scale, Window, WindowOptions};
 mod tileset;
 use tileset::*;
 
-type Framebuffer = [u32; WIDTH * HEIGHT];
+type Framebuffer = [u32; FB_WIDTH * FB_HEIGHT];
 
-const WIDTH: usize = 256;
-const HEIGHT: usize = 256;
+const WIDTH: usize = 32;
+const HEIGHT: usize = 32;
+
+const FB_WIDTH: usize = WIDTH * TILE_WIDTH;
+const FB_HEIGHT: usize = HEIGHT * TILE_HEIGHT;
 
 fn rgb(r: u8, g: u8, b: u8) -> u32 {
     let r = r as u32;
@@ -21,8 +24,16 @@ fn rgb(r: u8, g: u8, b: u8) -> u32 {
     r << 16 | g << 8 | b
 }
 
+#[derive(Copy, Clone, Debug)]
+struct ColouredTile {
+    tile_index: u8,
+    fg: u32,
+    bg: u32,
+}
+
 struct Demo {
     framebuffer: Framebuffer,
+    tilebuffer: [ColouredTile; WIDTH * HEIGHT],
     tiles: Tileset,
     is_running: bool,
     clock: GameClock,
@@ -32,7 +43,12 @@ struct Demo {
 impl Demo {
     fn new() -> Demo {
         Demo {
-            framebuffer: [0; WIDTH * HEIGHT],
+            framebuffer: [0; FB_WIDTH * FB_HEIGHT],
+            tilebuffer: [ColouredTile {
+                tile_index: 0,
+                fg: 0x00FFFFFF,
+                bg: 0,
+            }; FB_WIDTH / TILE_WIDTH * FB_HEIGHT / TILE_HEIGHT],
             tiles: gen_tileset(),
             is_running: true,
             clock: GameClock::new(),
@@ -43,9 +59,19 @@ impl Demo {
     fn step(&mut self, window: &mut Window) {
         let time = self.clock.tick(&VariableStep::new());
         self.counter.tick(&time);
-        self.draw((time.frame_number()) as usize);
+        self.draw();
+        self.tilebuffer = [ColouredTile {
+            tile_index: 16,
+            bg: 0x00FFFFFF,
+            fg: 0,
+        }; FB_WIDTH / TILE_WIDTH * FB_HEIGHT / TILE_HEIGHT];
+        for (i, tile) in self.tilebuffer.iter_mut().enumerate() {
+            let x = ((i % (FB_WIDTH / TILE_WIDTH)) * 8) as u8;
+            let y = ((i / (FB_WIDTH / TILE_WIDTH)) * 8) as u8;
+            tile.fg = rgb(x, y, x / 2 + y / 2);
+        }
         window.update_with_buffer(&self.framebuffer).unwrap();
-        if window.is_key_down(Key::Q) {
+        if window.is_key_down(Key::Q) || window.is_key_down(Key::Escape) {
             self.is_running = false;
         }
         self.clock.sleep_remaining(&self.counter);
@@ -55,16 +81,16 @@ impl Demo {
         self.is_running
     }
 
-    fn draw(&mut self, _time: usize) {
-        for y in 0..16 {
-            for x in 0..16 {
+    fn draw(&mut self) {
+        for (y, row) in self.tilebuffer.chunks(FB_WIDTH / TILE_WIDTH).enumerate() {
+            for (x, tile) in row.iter().enumerate() {
                 show_tile(
                     &mut self.framebuffer,
                     x,
                     y,
-                    self.tiles[x + y * 16],
-                    rgb(255, 255, 255),
-                    rgb(0, 128, 255),
+                    self.tiles[tile.tile_index as usize],
+                    tile.fg,
+                    tile.bg,
                 );
             }
         }
@@ -76,7 +102,7 @@ fn main() {
         scale: Scale::X2,
         ..Default::default()
     };
-    let mut window = Window::new("minidemo", WIDTH, HEIGHT, options).unwrap();
+    let mut window = Window::new("minidemo", FB_WIDTH, FB_HEIGHT, options).unwrap();
     let mut demo = Demo::new();
     while window.is_open() && demo.is_running() {
         demo.step(&mut window);
--- src/tileset.rs
+++ src/tileset.rs
@@ -1,34 +1,50 @@
 use Framebuffer;
-use WIDTH;
+use FB_WIDTH;
 
-pub type Tile = [bool; 8 * 8];
-pub type Tileset = [Tile; 16 * 16];
+pub const TILE_WIDTH: usize = 8;
+pub const TILE_HEIGHT: usize = 8;
 
-const RAW_TILES: &'static [u8; 2048] = include_bytes!("../data/tiles.raw");
+const TILESET_WIDTH: usize = 16;
+const TILESET_HEIGHT: usize = 16;
+const PIXELS_PER_BYTE: usize = 8;
+
+pub type Tile = [bool; TILE_WIDTH * TILE_HEIGHT];
+pub type Tileset = [Tile; TILESET_WIDTH * TILESET_HEIGHT];
+
+const RAW_TILES: &'static [u8; TILESET_WIDTH * TILESET_HEIGHT * TILE_WIDTH * TILE_HEIGHT
+             / PIXELS_PER_BYTE] = include_bytes!("../data/tiles.raw");
+
 fn tile_pixel(x: usize, y: usize) -> bool {
     let xlow = x & 0x07;
     let xhigh = x >> 3;
-    ((RAW_TILES[xhigh + y * 16] << xlow) & 0x80) != 0
+    ((RAW_TILES[xhigh + y * TILESET_WIDTH] << xlow) & 0x80) != 0
 }
 
 pub fn gen_tileset() -> Tileset {
-    let mut tiles = [[false; 8 * 8]; 16 * 16];
+    let mut tiles = [[false; TILE_WIDTH * TILE_HEIGHT]; TILESET_WIDTH * TILESET_HEIGHT];
     for y in 0..128 {
         for x in 0..128 {
             let (xh, yh) = (x / 8, y / 8);
             let (xl, yl) = (x & 0x07, y & 0x07);
-            tiles[xh + yh * 16][xl + yl * 8] = tile_pixel(x, y);
+            tiles[xh + yh * TILESET_WIDTH][xl + yl * TILE_WIDTH] = tile_pixel(x, y);
         }
     }
     tiles
 }
 
-pub fn show_tile(buffer: &mut Framebuffer, mut x: usize, mut y: usize, tile: Tile, fg: u32, bg: u32) {
-    x *= 8;
-    y *= 8;
-    for tx in 0..8 {
-        for ty in 0..8 {
-            buffer[x + tx + (y + ty) * WIDTH] = if tile[tx + ty * 8] { fg } else { bg };
+pub fn show_tile(
+    buffer: &mut Framebuffer,
+    mut x: usize,
+    mut y: usize,
+    tile: Tile,
+    fg: u32,
+    bg: u32,
+) {
+    x *= TILE_WIDTH;
+    y *= TILE_HEIGHT;
+    for tx in 0..TILE_WIDTH {
+        for ty in 0..TILE_HEIGHT {
+            buffer[x + tx + (y + ty) * FB_WIDTH] = if tile[tx + ty * TILE_WIDTH] { fg } else { bg };
         }
     }
 }
