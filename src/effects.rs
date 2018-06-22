30b7e7d5fedd4d445b77c15dacfe43829ad60c1c
Filip Szczepański <jazz2rulez@gmail.com>
2018-06-22 22:44:24 +0200

    Flower effect

--- src/effects.rs
+++ src/effects.rs
@@ -1,35 +1,84 @@
+use std;
+
 use {rgb, ColouredTile, Tilebuffer, HEIGHT, WIDTH};
+
+use util::{Texture, Vec2, Vec4};
 
 use rand::random;
 
+trait Effect {
+    fn step(&mut self, tilebuffer: &mut Tilebuffer, time: u64);
+}
+
 pub struct Sequencer {
-    fire: [u8; WIDTH * HEIGHT],
+    flower: Flower,
 }
 
 impl Sequencer {
     pub fn new() -> Sequencer {
         Sequencer {
-            fire: [0; WIDTH * HEIGHT],
+            flower: Flower::new(),
         }
     }
 
     pub fn step(&mut self, tilebuffer: &mut Tilebuffer, time: u64) {
-        for x in 0..WIDTH {
-            self.fire[x + (HEIGHT - 1) * WIDTH] = random();
+        self.flower.step(tilebuffer, time);
+    }
+}
+
+struct Flower {
+    colours: [u32; 10],
+    init: bool,
+}
+
+impl Flower {
+    fn new() -> Flower {
+        let mut colours = [0; 10];
+        for colour in &mut colours[..] {
+            *colour = random();
         }
-        for y in (0..HEIGHT - 1).rev() {
-            for x in 1..(WIDTH - 1) {
-                self.fire[x + y * WIDTH] = self.fire[x - 1 + (y + 1) * WIDTH] / 3
-                    + self.fire[x + (y + 1) * WIDTH] / 3
-                    + self.fire[x + 1 + (y + 1) * WIDTH] / 3;
+        Flower {
+            colours,
+            init: false,
+        }
+    }
+}
+
+impl Effect for Flower {
+    fn step(&mut self, buffer: &mut Tilebuffer, time: u64) {
+        if !self.init {
+            for x in 0..WIDTH / 2 {
+                for y in 0..HEIGHT / 2 {
+                    buffer[x + y * WIDTH].index = 2;
+                }
+                for y in HEIGHT / 2..HEIGHT {
+                    buffer[x + y * WIDTH].index = 18;
+                }
             }
-        }
-        for (i, bg) in self.fire.iter().enumerate() {
-            tilebuffer[i] = ColouredTile {
-                tile_index: b' ',
-                fg: 0,
-                bg: rgb(*bg,20,0),
-            };
+            for x in WIDTH / 2..WIDTH {
+                for y in 0..HEIGHT / 2 {
+                    buffer[x + y * WIDTH].index = 3;
+                }
+                for y in HEIGHT / 2..HEIGHT {
+                    buffer[x + y * WIDTH].index = 19;
+                }
+            }
+            for x in 0..WIDTH {
+                for y in 0..HEIGHT {
+                    let ColouredTile {
+                        ref mut fg,
+                        ref mut bg,
+                        ..
+                    } = buffer[x + y * WIDTH];
+                    *fg = rgb(255, 255, 255);
+                    *bg = rgb(0, 0, 0);
+                    let checkerboard = x ^ y ^ (x >> 4) ^ (y >> 4);
+                    if (checkerboard & 1) != 0 {
+                        std::mem::swap(fg, bg);
+                    }
+                }
+            }
+            self.init = true;
         }
     }
 }
